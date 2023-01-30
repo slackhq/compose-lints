5868682e5366d12032f94362210703fa49d84568
Zac Sweers <pandanomic@gmail.com>
2023-01-30 17:30:34 -0500

    Add unique IDs test
    Example failure:
    
    ```
    Duplicate issue IDs found!
    ComposeModifierWithoutDefault=[ComposeModifierWithoutDefaultDetector, ComposeUnstableCollectionsDetector]
    ```

---
+++ compose-lint-checks/src/test/java/slack/lint/compose/ComposeLintsIssueRegistryTest.kt
@@ -0,0 +1,25 @@
+// Copyright (C) 2023 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package slack.lint.compose
+
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Issue
+import org.junit.Test
+
+class ComposeLintsIssueRegistryTest : BaseSlackLintTest() {
+  @Test
+  fun ensureUniqueIds() {
+    val issues = ComposeLintsIssueRegistry().issues
+    if (issues.distinctBy { it.id }.size != issues.size) {
+      fail("Duplicate issue IDs found!\n${issues.groupBy { it.id }.filter { it.value.size > 1 }.entries.joinToString("\n") { (key, value) -> "${key}=${value.map { it.implementation.detectorClass.simpleName }}" } }")
+    }
+  }
+
+  override fun getDetector(): Detector {
+    throw NotImplementedError()
+  }
+
+  override fun getIssues(): List<Issue> {
+    throw NotImplementedError()
+  }
+}
