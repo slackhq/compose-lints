c01b1a91e16dd6cd95e6d5f6b1623f11ff61b6c1
Zac Sweers <pandanomic@gmail.com>
2023-01-30 17:26:51 -0500

    Fix package name for registry

--- compose-lint-checks/src/main/java/slack/lint/compose/ComposeLintsIssueRegistry.kt
+++ compose-lint-checks/src/main/java/slack/lint/compose/ComposeLintsIssueRegistry.kt
@@ -1,26 +1,12 @@
 // Copyright (C) 2023 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
-package slack.lint
+package slack.lint.compose
 
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 import com.google.auto.service.AutoService
-import slack.lint.compose.ComposeCompositionLocalNamingDetector
-import slack.lint.compose.ComposeCompositionLocalUsageDetector
-import slack.lint.compose.ComposeContentEmitterReturningValuesDetector
-import slack.lint.compose.ComposeModifierMissingDetector
-import slack.lint.compose.ComposeModifierReusedDetector
-import slack.lint.compose.ComposeModifierWithoutDefaultDetector
-import slack.lint.compose.ComposeMultipleContentEmittersDetector
-import slack.lint.compose.ComposeMutableParametersDetector
-import slack.lint.compose.ComposeNamingDetector
-import slack.lint.compose.ComposeParameterOrderDetector
-import slack.lint.compose.ComposePreviewNamingDetector
-import slack.lint.compose.ComposePreviewPublicDetector
-import slack.lint.compose.ComposeRememberMissingDetector
-import slack.lint.compose.ComposeUnstableCollectionsDetector
 
 @AutoService(IssueRegistry::class)
 class ComposeLintsIssueRegistry : IssueRegistry() {
