65ec781519cec4fdaa9c5907ad7b53f5e1941389
Zac Sweers <pandanomic@gmail.com>
2023-02-09 12:13:41 -0500

    Introduce spotless-external to preserve original licenses

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,56 @@ plugins {
 
 val ktfmtVersion = libs.versions.ktfmt.get()
 
+val pathPrefix = "compose-lint-checks/src/main/java/slack/lint/compose"
+val utilPathPrefix = "$pathPrefix/util"
+val testPathPrefix = "compose-lint-checks/src/test/java/slack/lint/compose"
+val externalFiles =
+  arrayOf(
+    "$utilPathPrefix/ASTNodes.kt",
+    "$utilPathPrefix/BooleanLintOption",
+    "$utilPathPrefix/Composables.kt",
+    "$utilPathPrefix/KotlinUtils.kt",
+    "$utilPathPrefix/KtAnnotateds.kt",
+    "$utilPathPrefix/KtCallableDeclarations.kt",
+    "$utilPathPrefix/KtFunctions.kt",
+    "$utilPathPrefix/Previews.kt",
+    "$utilPathPrefix/Priorities",
+    "$utilPathPrefix/PsiElements.kt",
+    "$pathPrefix/ComposableFunctionDetector.kt",
+    "$pathPrefix/ComposableFunctionNamingDetector.kt",
+    "$pathPrefix/CompositionLocalUsageDetector.kt",
+    "$pathPrefix/ContentEmitterReturningValuesDetector.kt",
+    "$pathPrefix/ModifierComposableDetector.kt",
+    "$pathPrefix/ModifierMissingDetector.kt",
+    "$pathPrefix/ModifierReusedDetector.kt",
+    "$pathPrefix/ModifierWithoutDefaultDetector.kt",
+    "$pathPrefix/MultipleContentEmittersDetector.kt",
+    "$pathPrefix/MutableParametersDetector.kt",
+    "$pathPrefix/ParameterOrderDetector.kt",
+    "$pathPrefix/PreviewNamingDetector.kt",
+    "$pathPrefix/PreviewPublicDetector.kt",
+    "$pathPrefix/RememberMissingDetector.kt",
+    "$pathPrefix/UnstableCollectionsDetector.kt",
+    "$pathPrefix/ViewModelForwardingDetector.kt",
+    "$pathPrefix/ViewModelInjectionDetector.kt",
+    "$testPathPrefix/ComposableFunctionNamingDetectorTest.kt",
+    "$testPathPrefix/CompositionLocalUsageDetectorTest.kt",
+    "$testPathPrefix/ContentEmitterReturningValuesDetectorTest.kt",
+    "$testPathPrefix/ModifierComposableDetectorTest.kt",
+    "$testPathPrefix/ModifierMissingDetectorTest.kt",
+    "$testPathPrefix/ModifierReusedDetectorTest.kt",
+    "$testPathPrefix/ModifierWithoutDefaultDetectorTest.kt",
+    "$testPathPrefix/MultipleContentEmittersDetectorTest.kt",
+    "$testPathPrefix/MutableParametersDetectorTest.kt",
+    "$testPathPrefix/ParameterOrderDetectorTest.kt",
+    "$testPathPrefix/PreviewNamingDetectorTest.kt",
+    "$testPathPrefix/PreviewPublicDetectorTest.kt",
+    "$testPathPrefix/RememberMissingDetectorTest.kt",
+    "$testPathPrefix/UnstableCollectionsDetectorTest.kt",
+    "$testPathPrefix/ViewModelForwardingDetectorTest.kt",
+    "$testPathPrefix/ViewModelInjectionDetectorTest.kt",
+  )
+
 allprojects {
   apply(plugin = "com.diffplug.spotless")
   configure<SpotlessExtension> {
@@ -32,14 +82,15 @@ allprojects {
       trimTrailingWhitespace()
       endWithNewline()
       licenseHeaderFile(rootProject.file("spotless/spotless.kt"))
-      targetExclude("**/spotless.kt", "**/denylistedapis/*.kt")
+      targetExclude("spotless/*.kt", "**/spotless/*.kt", *externalFiles)
     }
     // Externally adapted sources that should preserve their license header
     format("kotlinExternal", KotlinExtension::class.java) {
-      target("**/denylistedapis/*.kt")
+      target(*externalFiles)
       ktfmt(ktfmtVersion).googleStyle()
       trimTrailingWhitespace()
       endWithNewline()
+      licenseHeaderFile(rootProject.file("spotless/spotless-external.kt"))
     }
     kotlinGradle {
       ktfmt(ktfmtVersion).googleStyle()
---
+++ spotless/spotless-external.kt
@@ -0,0 +1,4 @@
+// Copyright (C) $YEAR Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Twitter, Inc.
+// SPDX-License-Identifier: Apache-2.0
