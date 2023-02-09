1ccc72ea9d6418d1220480f988e21c85214bee82
Zac Sweers <pandanomic@gmail.com>
2023-02-09 12:33:06 -0500

    Introduce spotless-external to preserve original licenses (#42)
    * Introduce spotless-external to preserve original licenses
    
    * Re-run spotless
    
    * Fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,56 @@ plugins {
 
 val ktfmtVersion = libs.versions.ktfmt.get()
 
+val pathPrefix = "src/main/java/slack/lint/compose"
+val utilPathPrefix = "$pathPrefix/util"
+val testPathPrefix = "src/test/java/slack/lint/compose"
+val externalFiles =
+  arrayOf(
+    "$utilPathPrefix/ASTNodes.kt",
+    "$utilPathPrefix/BooleanLintOption",
+    "$utilPathPrefix/Composables.kt",
+    "$utilPathPrefix/KotlinUtils.kt",
+    "$utilPathPrefix/KtAnnotateds.kt",
+    "$utilPathPrefix/KtCallableDeclarations.kt",
+    "$utilPathPrefix/KtFunctions.kt",
+    "$utilPathPrefix/Previews.kt",
+    "$utilPathPrefix/Priorities",
+    "$utilPathPrefix/PsiElements.kt",
+    "$pathPrefix/ComposableFunctionDetector.kt",
+    "$pathPrefix/ComposableFunctionNamingDetector.kt",
+    "$pathPrefix/CompositionLocalUsageDetector.kt",
+    "$pathPrefix/ContentEmitterReturningValuesDetector.kt",
+    "$pathPrefix/ModifierComposableDetector.kt",
+    "$pathPrefix/ModifierMissingDetector.kt",
+    "$pathPrefix/ModifierReusedDetector.kt",
+    "$pathPrefix/ModifierWithoutDefaultDetector.kt",
+    "$pathPrefix/MultipleContentEmittersDetector.kt",
+    "$pathPrefix/MutableParametersDetector.kt",
+    "$pathPrefix/ParameterOrderDetector.kt",
+    "$pathPrefix/PreviewNamingDetector.kt",
+    "$pathPrefix/PreviewPublicDetector.kt",
+    "$pathPrefix/RememberMissingDetector.kt",
+    "$pathPrefix/UnstableCollectionsDetector.kt",
+    "$pathPrefix/ViewModelForwardingDetector.kt",
+    "$pathPrefix/ViewModelInjectionDetector.kt",
+    "$testPathPrefix/ComposableFunctionNamingDetectorTest.kt",
+    "$testPathPrefix/CompositionLocalUsageDetectorTest.kt",
+    "$testPathPrefix/ContentEmitterReturningValuesDetectorTest.kt",
+    "$testPathPrefix/ModifierComposableDetectorTest.kt",
+    "$testPathPrefix/ModifierMissingDetectorTest.kt",
+    "$testPathPrefix/ModifierReusedDetectorTest.kt",
+    "$testPathPrefix/ModifierWithoutDefaultDetectorTest.kt",
+    "$testPathPrefix/MultipleContentEmittersDetectorTest.kt",
+    "$testPathPrefix/MutableParametersDetectorTest.kt",
+    "$testPathPrefix/ParameterOrderDetectorTest.kt",
+    "$testPathPrefix/PreviewNamingDetectorTest.kt",
+    "$testPathPrefix/PreviewPublicDetectorTest.kt",
+    "$testPathPrefix/RememberMissingDetectorTest.kt",
+    "$testPathPrefix/UnstableCollectionsDetectorTest.kt",
+    "$testPathPrefix/ViewModelForwardingDetectorTest.kt",
+    "$testPathPrefix/ViewModelInjectionDetectorTest.kt",
+  )
+
 allprojects {
   apply(plugin = "com.diffplug.spotless")
   configure<SpotlessExtension> {
@@ -27,19 +77,20 @@ allprojects {
       endWithNewline()
     }
     kotlin {
-      target("**/*.kt")
+      target("src/**/*.kt")
       ktfmt(ktfmtVersion).googleStyle()
       trimTrailingWhitespace()
       endWithNewline()
       licenseHeaderFile(rootProject.file("spotless/spotless.kt"))
-      targetExclude("**/spotless.kt", "**/denylistedapis/*.kt")
+      targetExclude("spotless/*.kt", "**/spotless/*.kt", *externalFiles)
     }
     // Externally adapted sources that should preserve their license header
     format("kotlinExternal", KotlinExtension::class.java) {
-      target("**/denylistedapis/*.kt")
+      target(*externalFiles)
       ktfmt(ktfmtVersion).googleStyle()
       trimTrailingWhitespace()
       endWithNewline()
+      licenseHeaderFile(rootProject.file("spotless/spotless-external.kt"))
     }
     kotlinGradle {
       ktfmt(ktfmtVersion).googleStyle()
--- compose-lint-checks/src/main/java/slack/lint/compose/util/KtAnnotateds.kt
+++ compose-lint-checks/src/main/java/slack/lint/compose/util/KtAnnotateds.kt
@@ -1,5 +1,7 @@
 // Copyright (C) 2022 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Twitter, Inc.
+// SPDX-License-Identifier: Apache-2.0
 package slack.lint.compose.util
 
 import org.jetbrains.kotlin.psi.KtAnnotated
