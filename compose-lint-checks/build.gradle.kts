dfb90aa09befb7a5a8276df56b91efe7f7e5018f
Zac Sweers <pandanomic@gmail.com>
2024-01-21 18:13:54 -0500

    Target kotlin 1.9 to align with lint

--- compose-lint-checks/build.gradle.kts
+++ compose-lint-checks/build.gradle.kts
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 Salesforce, Inc.
 // SPDX-License-Identifier: Apache-2.0
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -18,11 +19,12 @@ lint {
   checkTestSources = true
   baseline = file("lint-baseline.xml")
 
-  fatal += setOf(
-    "LintDocExample",
-    "LintImplPsiEquals",
-    "LintImplUnexpectedDomain",
-  )
+  fatal +=
+    setOf(
+      "LintDocExample",
+      "LintImplPsiEquals",
+      "LintImplUnexpectedDomain",
+    )
 }
 
 dependencies {
@@ -33,11 +35,14 @@ dependencies {
   testImplementation(libs.junit)
 }
 
+val kgpKotlinVersion = KotlinVersion.KOTLIN_1_9
+
 tasks.withType<KotlinCompile>().configureEach {
-  kotlinOptions {
+  compilerOptions {
     // Lint forces Kotlin (regardless of what version the project uses), so this
-    // forces a lower language level for now. Similar to `targetCompatibility` for Java.
-    apiVersion = "1.7"
-    languageVersion = "1.7"
+    // forces a matching language level for now. Similar to `targetCompatibility` for Java.
+    // This should match the value in LintKotlinVersionCheckTest.kt
+    apiVersion.set(kgpKotlinVersion)
+    languageVersion.set(kgpKotlinVersion)
   }
 }
