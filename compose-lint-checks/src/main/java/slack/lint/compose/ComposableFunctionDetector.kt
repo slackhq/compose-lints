03647336fd145c0763029a407e53b80e8e062a8b
Zac Sweers <pandanomic@gmail.com>
2024-01-23 02:29:10 -0500

    Add intermediary composable function overrides

--- compose-lint-checks/src/main/java/slack/lint/compose/ComposableFunctionDetector.kt
+++ compose-lint-checks/src/main/java/slack/lint/compose/ComposableFunctionDetector.kt
@@ -8,6 +8,7 @@ import com.android.tools.lint.detector.api.JavaContext
 import com.android.tools.lint.detector.api.SourceCodeScanner
 import com.android.tools.lint.detector.api.isKotlin
 import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
 import org.jetbrains.uast.UMethod
 import slack.lint.compose.util.LintOption
 import slack.lint.compose.util.OptionLoadingDetector
@@ -23,12 +24,28 @@ abstract class ComposableFunctionDetector(vararg options: LintOption) :
     return object : UElementHandler() {
       override fun visitMethod(node: UMethod) {
         if (node.isComposable) {
-          val ktFunction = node.sourcePsi as? KtFunction ?: return
-          visitComposable(context, node, ktFunction)
+          visitComposable(context, node)
+          when (val sourcePsi = node.sourcePsi ?: return) {
+            is KtPropertyAccessor -> {
+              visitComposable(context, node, sourcePsi)
+            }
+
+            is KtFunction -> {
+              visitComposable(context, node, sourcePsi)
+            }
+          }
         }
       }
     }
   }
 
-  abstract fun visitComposable(context: JavaContext, method: UMethod, function: KtFunction)
+  open fun visitComposable(context: JavaContext, method: UMethod) {
+  }
+
+  open fun visitComposable(context: JavaContext, method: UMethod, property: KtPropertyAccessor) {
+  }
+
+  open fun visitComposable(context: JavaContext, method: UMethod, function: KtFunction) {
+
+  }
 }
