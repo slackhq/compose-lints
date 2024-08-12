6653c4e71f4cded79bc01f10e892987aef10f07e
Alex Vanyo <vanyo@google.com>
2024-08-11 19:36:11 -0700

    Implement SlotReusedDetector

---
+++ compose-lint-checks/src/main/java/slack/lint/compose/SlotReusedDetector.kt
@@ -0,0 +1,103 @@
+// Copyright (C) 2024 Salesforce, Inc.
+// SPDX-License-Identifier: Apache-2.0
+package slack.lint.compose
+
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.android.tools.lint.detector.api.TextFormat
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtLambdaExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.psiUtil.isAncestor
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UMethod
+import slack.lint.compose.util.Priorities
+import slack.lint.compose.util.emitsContent
+import slack.lint.compose.util.findChildrenByClass
+import slack.lint.compose.util.slotParameters
+import slack.lint.compose.util.sourceImplementation
+
+class SlotReusedDetector
+@JvmOverloads
+constructor(
+  private val contentEmitterOption: ContentEmitterLintOption =
+    ContentEmitterLintOption(CONTENT_EMITTER_OPTION)
+) : ComposableFunctionDetector(contentEmitterOption), SourceCodeScanner {
+
+  companion object {
+
+    val CONTENT_EMITTER_OPTION = ContentEmitterLintOption.newOption()
+
+    val ISSUE =
+      Issue.create(
+          id = "SlotReused",
+          briefDescription = "Slots should be invoked in at most one place",
+          explanation =
+            """
+            Slots should be invoked in at most once place to meet lifecycle expectations. \
+            Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. \
+            Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. \
+            See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information.
+          """,
+          category = Category.CORRECTNESS,
+          priority = Priorities.NORMAL,
+          severity = Severity.ERROR,
+          implementation = sourceImplementation<SlotReusedDetector>(),
+        )
+        .setOptions(listOf(CONTENT_EMITTER_OPTION))
+  }
+
+  override fun visitComposable(context: JavaContext, method: UMethod, function: KtFunction) {
+    if (!function.emitsContent(contentEmitterOption.value)) return
+    val composableBlockExpression = function.bodyBlockExpression ?: return
+    val slotParameters = method.slotParameters(context.evaluator)
+
+    val callExpressions = composableBlockExpression.findChildrenByClass<KtCallExpression>().toList()
+    val innerLambdas = composableBlockExpression.findChildrenByClass<KtLambdaExpression>().toList()
+
+    slotParameters.forEach { slotParameter ->
+      // Find lambdas that shadow this parameter name, to make sure that they aren't shadowing
+      // the references we are looking through
+      val lambdasWithMatchingParameterName =
+        innerLambdas.filter { innerLambda ->
+          // Otherwise look to see if any of the parameters on the inner lambda have the
+          // same name
+          innerLambda.valueParameters
+            // Ignore parameters with a destructuring declaration instead of a named
+            // parameter
+            .filter { it.destructuringDeclaration == null }
+            .any { it.name == slotParameter.name }
+        }
+
+      // Count all direct calls of the slot parameter.
+      // NOTE: this misses cases where the slot parameter is passed as an argument to another
+      // method, which may or may not invoke the slot parameter, but there are cases where that is
+      // valid, like using the slot parameter as the key for a remember
+      val slotParameterCallCount =
+        callExpressions
+          .filter { reference ->
+            // The parameter is referenced if there is at least one reference that isn't shadowed by
+            // an inner lambda
+            lambdasWithMatchingParameterName.none { it.isAncestor(reference) }
+          }
+          .count { reference ->
+            (reference.calleeExpression as? KtNameReferenceExpression)?.getReferencedName() ==
+              slotParameter.name
+          }
+
+      // Report an issue if the slot parameter was invoked in multiple places
+      if (slotParameterCallCount > 1) {
+        context.report(
+          ISSUE,
+          slotParameter as UElement,
+          context.getLocation(slotParameter as UElement),
+          ISSUE.getExplanation(TextFormat.TEXT),
+        )
+      }
+    }
+  }
+}
---
+++ compose-lint-checks/src/test/java/slack/lint/compose/SlotReusedDetectorTest.kt
@@ -0,0 +1,293 @@
+// Copyright (C) 2024 Salesforce, Inc.
+// SPDX-License-Identifier: Apache-2.0
+package slack.lint.compose
+
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Issue
+import org.intellij.lang.annotations.Language
+import org.junit.Test
+
+class SlotReusedDetectorTest : BaseComposeLintTest() {
+
+  override fun getDetector(): Detector = SlotReusedDetector()
+
+  override fun getIssues(): List<Issue> = listOf(SlotReusedDetector.ISSUE)
+
+  @Test
+  fun `errors when the slot parameter of a Composable is used more than once at the same time`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          slot: @Composable () -> Unit,
+        ) {
+          Row(modifier) {
+            slot()
+            slot()
+          }
+        }
+
+        @Composable
+        fun SomethingElse(
+          modifier: Modifier = Modifier,
+          slot: @Composable () -> Unit,
+        ) {
+          Column(modifier) {
+            slot()
+            Box {
+              slot()
+            }
+          }
+        }
+      """
+        .trimIndent()
+
+    lint()
+      .files(*commonStubs, kotlin(code))
+      .run()
+      .expect(
+        """
+          src/test.kt:7: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          src/test.kt:18: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          2 errors, 0 warnings
+        """
+          .trimIndent()
+      )
+  }
+
+  @Test
+  fun `errors when the slot parameter of a Composable is used more than once in different branches`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot: @Composable () -> Unit,
+        ) {
+          Box(modifier) {
+            if (flag) {
+              slot()
+            } else {
+              slot()
+            }
+          }
+        }
+      """
+        .trimIndent()
+
+    lint()
+      .files(*commonStubs, kotlin(code))
+      .run()
+      .expect(
+        """
+          src/test.kt:8: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          1 errors, 0 warnings
+        """
+          .trimIndent()
+      )
+  }
+
+  @Test
+  fun `errors when slot parameter is used in different movableContentOfs`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot: @Composable () -> Unit,
+        ) {
+          val movableSlot1 = remember(slot) { movableContentOf { slot() } }
+          val movableSlot2 = remember(slot) { movableContentOf { slot() } }
+          Box(modifier) {
+            if (flag) {
+              movableSlot1()
+            } else {
+              movableSlot2()
+            }
+          }
+        }
+      """
+        .trimIndent()
+
+    lint()
+      .files(*commonStubs, kotlin(code))
+      .run()
+      .expect(
+        """
+          src/test.kt:8: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          1 errors, 0 warnings
+        """
+          .trimIndent()
+      )
+  }
+
+  @Test
+  fun `errors when multiple slot parameters of a Composable are used more than once in different branches`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot1: @Composable () -> Unit,
+          slot2: @Composable () -> Unit,
+        ) {
+          Box(modifier) {
+            if (flag) {
+              slot1()
+              slot2()
+            } else {
+              slot1()
+              slot2()
+            }
+          }
+        }
+      """
+        .trimIndent()
+
+    lint()
+      .files(*commonStubs, kotlin(code))
+      .run()
+      .expect(
+        """
+          src/test.kt:8: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot1: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          src/test.kt:9: Error: Slots should be invoked in at most once place to meet lifecycle expectations. Slots should not be invoked multiple times in the same composition, as callers don't expect the slot's contents to be duplicated in multiple places. Slots should also not be invoked in multiple places in source code, where the invoking location changes based on some condition. This will preserve the slot's internal state when the invoking location changes. See https://slackhq.github.io/compose-lints/rules/#do-not-invoke-slots-in-more-than-once-place for more information. [SlotReused]
+            slot2: @Composable () -> Unit,
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          2 errors, 0 warnings
+        """
+          .trimIndent()
+      )
+  }
+
+  @Test
+  fun `passes when the slot parameter is shadowed`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot: @Composable () -> Unit,
+        ) {
+          Box(modifier) {
+            slot()
+            SomethingElse { slot ->
+              slot()
+            }
+          }
+        }
+
+        @Composable
+        fun SomethingElse(
+          modifier: Modifier = Modifier,
+          content: @Composable (@Composable () -> Unit) -> Unit,
+        ) {
+          Box(modifier) {
+            content {
+              Spacer()
+            }
+          }
+        }
+
+
+      """
+        .trimIndent()
+
+    lint().files(*commonStubs, kotlin(code)).run().expectClean()
+  }
+
+  @Test
+  fun `passes when using movableContentOf for the slot parameter`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.runtime.movableContentOf
+        import androidx.compose.runtime.remember
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot: @Composable () -> Unit,
+        ) {
+          val movableSlot = remember(slot) { movableContentOf(slot) }
+          Box(modifier) {
+            if (flag) {
+              movableSlot()
+            } else {
+              movableSlot()
+            }
+          }
+        }
+
+      """
+        .trimIndent()
+
+    lint().files(*commonStubs, kotlin(code)).run().expectClean()
+  }
+
+  @Test
+  fun `passes when using slot parameter in only one branch`() {
+    @Language("kotlin")
+    val code =
+      """
+        import androidx.compose.runtime.Composable
+        import androidx.compose.ui.Modifier
+
+        @Composable
+        fun Something(
+          modifier: Modifier = Modifier,
+          flag: Boolean,
+          slot: @Composable () -> Unit,
+        ) {
+          Box(modifier) {
+            if (flag) {
+              slot()
+            } else {
+              Spacer()
+            }
+          }
+        }
+
+      """
+        .trimIndent()
+
+    lint().files(*commonStubs, kotlin(code)).run().expectClean()
+  }
+}
