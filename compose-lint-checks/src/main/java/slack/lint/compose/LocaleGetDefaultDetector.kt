43e6af3000cec1be66293d68c09a4d2c3558552b
ansehun <ansehoon1999@gmail.com>
2025-03-08 16:23:38 +0900

    Apply lint check

--- compose-lint-checks/src/main/java/slack/lint/compose/LocaleGetDefaultDetector.kt
+++ compose-lint-checks/src/main/java/slack/lint/compose/LocaleGetDefaultDetector.kt
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 Salesforce, Inc.
+// SPDX-License-Identifier: Apache-2.0
 package slack.lint.compose
 
 import com.android.tools.lint.detector.api.Category
@@ -15,47 +17,43 @@ import slack.lint.compose.util.sourceImplementation
 
 class LocaleGetDefaultDetector : ComposableFunctionDetector(), SourceCodeScanner {
 
-    companion object {
-        const val JAVA_UTIL_LOCALE ="java.util.Locale"
+  companion object {
+    const val JAVA_UTIL_LOCALE = "java.util.Locale"
 
-        val ISSUE =
-            Issue.create(
-                id = "LocaleGetDefaultDetector",
-                briefDescription = "Avoid using `Locale.getDefault()` in Composable functions",
-                explanation =
-                """
-                    Using `Locale.getDefault()` in a @Composable function does not trigger recomposition when the locale changes (e.g., during a Configuration change). \  
-                    Instead, use `LocalConfiguration.current.locales`, which correctly updates UI and works better for previews and tests.  
+    val ISSUE =
+      Issue.create(
+        id = "LocaleGetDefaultDetector",
+        briefDescription = "Avoid using `Locale.getDefault()` in Composable functions",
+        explanation =
+          """
+                    Using `Locale.getDefault()` in a @Composable function does not trigger recomposition when the locale changes (e.g., during a Configuration change). \
+                    Instead, use `LocalConfiguration.current.locales`, which correctly updates UI and works better for previews and tests.
                 """,
-                category = Category.CORRECTNESS,
-                priority = Priorities.NORMAL,
-                severity = Severity.ERROR,
-                implementation = sourceImplementation<LocaleGetDefaultDetector>(),
-            )
-    }
+        category = Category.CORRECTNESS,
+        priority = Priorities.NORMAL,
+        severity = Severity.ERROR,
+        implementation = sourceImplementation<LocaleGetDefaultDetector>(),
+      )
+  }
 
-    override fun getApplicableMethodNames(): List<String> = listOf("getDefault")
+  override fun getApplicableMethodNames(): List<String> = listOf("getDefault")
 
+  override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
 
-    override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
+    if (!context.evaluator.isMemberInClass(method, JAVA_UTIL_LOCALE)) return
 
-        if (!context.evaluator.isMemberInClass(method, JAVA_UTIL_LOCALE)) return
+    val parentFunction = node.getParentOfType(UMethod::class.java) ?: return
 
-        val parentFunction = node.getParentOfType(UMethod::class.java) ?: return
-
-        if (parentFunction.isComposable) {
-            context.report(
-                ISSUE,
-                node,
-                context.getLocation(node),
-                """
+    if (parentFunction.isComposable) {
+      context.report(
+        ISSUE,
+        node,
+        context.getLocation(node),
+        """
                         Don't use `Locale.getDefault()` in a @Composable function.
                         Use `LocalConfiguration.current.locales` instead to properly handle locale changes."
-                     """
-            )
-        }
-
+                     """,
+      )
     }
-
-
+  }
 }
--- compose-lint-checks/src/test/java/slack/lint/compose/LocaleGetDefaultDetectorTest.kt
+++ compose-lint-checks/src/test/java/slack/lint/compose/LocaleGetDefaultDetectorTest.kt
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 Salesforce, Inc.
+// SPDX-License-Identifier: Apache-2.0
 package slack.lint.compose
 
 import com.android.tools.lint.checks.infrastructure.TestMode
@@ -8,36 +10,37 @@ import org.junit.Test
 
 class LocaleGetDefaultDetectorTest : BaseComposeLintTest() {
 
-    override fun getDetector(): Detector = LocaleGetDefaultDetector()
+  override fun getDetector(): Detector = LocaleGetDefaultDetector()
 
-    override fun getIssues(): List<Issue> = listOf(LocaleGetDefaultDetector.ISSUE)
+  override fun getIssues(): List<Issue> = listOf(LocaleGetDefaultDetector.ISSUE)
 
-    @Test
-    fun `errors when composable uses Locale_getDefault()`() {
-        @Language("kotlin")
-        val code =
-            """
+  @Test
+  fun `errors when composable uses Locale_getDefault()`() {
+    @Language("kotlin")
+    val code =
+      """
                 import java.util.Locale
                 import androidx.compose.runtime.Composable
-        
+
                 @Composable
                 fun Something() {
                     val locale = Locale.getDefault()
                 }
-            """.trimIndent()
-        lint()
-            .skipTestModes(TestMode.WHITESPACE, TestMode.IMPORT_ALIAS)
-            .files(*commonStubs, kotlin(code))
-            .run()
-            .expect(
-                """
+            """
+        .trimIndent()
+    lint()
+      .skipTestModes(TestMode.WHITESPACE, TestMode.IMPORT_ALIAS)
+      .files(*commonStubs, kotlin(code))
+      .run()
+      .expect(
+        """
                 src/test.kt:6: Error: Don't use Locale.getDefault() in a @Composable function.
                 Use LocalConfiguration.current.locales instead to properly handle locale changes." [LocaleGetDefaultDetector]
                     val locale = Locale.getDefault()
                                  ~~~~~~~~~~~~~~~~~~~
                 1 errors, 0 warnings
-                """.trimIndent()
-            )
-    }
+                """
+          .trimIndent()
+      )
+  }
 }
-
